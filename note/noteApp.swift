a713aef9bda424757a581cd6b451dfd21946df44
stevecrosby <info@stevecrosby.com>
2025-06-08 23:00:32 -0700

    Initial Commit

---
+++ note/noteApp.swift
@@ -0,0 +1,17 @@
+//
+//  noteApp.swift
+//  note
+//
+//  Created by Steven Crosby on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct noteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
